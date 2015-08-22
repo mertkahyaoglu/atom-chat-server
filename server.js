2f6564e4ad06e8afd5eab0cce829febdaff75e49
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-08-23 00:25:46 +0300

    added front page for server

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express    = require('express'),
     port       = process.env.PORT || 3000,
     chat       = require("./lib/chat");
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/public/views')
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.render('index');
+})
 var server = app.listen(port);
 chat.listen(server);
 console.log("Server running on: "+port);
